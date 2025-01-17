8297dce071d17e68d3e46033bd470505fbdb47ba
David Marx <david.marx84@gmail.com>
2025-01-17 14:16:45 -0800

    Create jest.config.js

---
+++ typescript/jest.config.js
@@ -0,0 +1,22 @@
+// jest.config.js
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFiles: ['./jest.setup.js'],
+  testMatch: ['<rootDir>/src/**/*.test.ts'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
+};
+
+// jest.setup.js
+require('jest-fetch-mock').enableMocks();
