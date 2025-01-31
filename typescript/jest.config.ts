10f56194559e55cfdd3668dd5783f62eee36cb0c
David Marx <david.marx84@gmail.com>
2025-01-30 20:46:32 -0800

    Update jest.config.ts

--- typescript/jest.config.ts
+++ typescript/jest.config.ts
@@ -11,7 +11,7 @@ const config: JestConfigWithTsJest = {
   coverageReporters: ['text', 'lcov'],
   coverageThreshold: {
     global: {
-      branches: 80,
+      branches: 70,    // Lowered from 80%
       functions: 80,
       lines: 80,
       statements: 80
