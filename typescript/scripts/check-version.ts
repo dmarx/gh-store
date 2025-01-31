ca6844687631c53854466acfda44b47296e8220d
David Marx <david.marx84@gmail.com>
2025-01-30 21:35:20 -0800

    Update check-version.ts

--- typescript/scripts/check-version.ts
+++ typescript/scripts/check-version.ts
@@ -1,6 +1,10 @@
 import { readFileSync } from 'fs';
-import { resolve } from 'path';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
 import { CLIENT_VERSION } from '../src/version';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const pkg = JSON.parse(readFileSync(resolve(__dirname, '../package.json'), 'utf8'));
 
