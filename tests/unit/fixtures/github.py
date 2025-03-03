3179e538774778c2ffd331eaa59add1f615e0406
David Marx <david.marx84@gmail.com>
2025-03-02 16:47:33 -0800

    Update github.py

--- tests/unit/fixtures/github.py
+++ tests/unit/fixtures/github.py
@@ -229,8 +229,8 @@ def mock_issue_factory(mock_comment_factory, mock_label_factory):
         issue.labels = issue_labels
         if labels:
             for label_name in labels:
-                #issue.labels.append(mock_label_factory(label_name))
-                issue.labels.append(label_name)
+                issue.labels.append(mock_label_factory(label_name))
+                #issue.labels.append(label_name)
         
         
         # Set up comments
